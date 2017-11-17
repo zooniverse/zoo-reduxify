0b00859d2298169c02e9b7c5f472e9361ef8ddf6
simensta <andrea@zooniverse.org>
2017-11-17 13:08:25 -0600

    add ZooHeader to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Route, Switch } from 'react-router-dom';
-import { ZooniverseLogo, ZooFooter } from 'zooniverse-react-components';
+import { ZooniverseLogo, ZooFooter, ZooHeader } from 'zooniverse-react-components';
 
 import AuthContainer from '../containers/AuthContainer';
 import AboutLayout from './about';
@@ -10,10 +10,10 @@ import Home from './Home';
 export default function App() {
   return (
     <div>
+      <ZooHeader authContainer={<AuthContainer />} />
       <header className="site-header">
         <Link to="/" className="link"><h1 className="title">Zooniverse Starter Project</h1></Link>
         <Link to="/about" className="link">About</Link>
-        <AuthContainer />
         <ZooniverseLogo />
       </header>
       <section className="content-section">

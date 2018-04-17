5aa4fd0923084059c77bdbba5a091a84d17d4de2
simensta <andrea@zooniverse.org>
2018-04-17 15:02:13 +0100

    Adds reducers for the the project and workflow resources

--- src/components/classifierDemo/index.jsx
+++ src/components/classifierDemo/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ZOODATA_INITIAL_STATE } from '../../ducks/zooniverse-data';
+import { connect } from 'react-redux';
+import { ZOODATA_INITIAL_STATE, ZOODATA_STATUS, ZooData } from '../../ducks/zooniverse-data';
 
 class ClassifierDemo extends React.Component {
   componentDidMount(){
     // TODO: fetch the Project data
+    if (this.props.zooProjectStatus === ZOODATA_STATUS.IDLE) {
+      this.props.dispatch(ZooData.fetchProject('1651'));
+    }
   }
 
   render() {
@@ -17,7 +21,13 @@ class ClassifierDemo extends React.Component {
             <button>New Subject</button>
             <button>Submit Classification</button>
           </div>
-          <div className="resource-status">Status of Project Resources</div>
+          <div className="resource-status">
+            <h2>Status of Project Resources</h2>
+            <p>Project Id: {this.props.zooProjectId}</p>
+            <p>Project Status: {this.props.zooProjectStatus}</p>
+            {this.props.zooProjectStatusMessage && (
+              <p>Project Error Message: {this.props.zooProjectStatusMessage}</p>)}
+          </div>
         </div>
       </div>
     );
@@ -26,8 +36,10 @@ class ClassifierDemo extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-
-
+    zooProjectId: state.zooniverseData.zooProjectId,
+    zooProjectData: state.zooniverseData.zooProjectData,
+    zooProjectStatus: state.zooniverseData.zooProjectStatus,
+    zooProjectStatusMessage: state.zooniverseData.zooProjectStatusMessage
   };
 };
 
@@ -46,4 +58,4 @@ ClassifierDemo.propTypes = {
 
 
 
-export default ClassifierDemo;
+export default connect(mapStateToProps)(ClassifierDemo);
--- src/ducks/zooniverse-data.js
+++ src/ducks/zooniverse-data.js
@@ -17,13 +17,16 @@ import apiClient from 'panoptes-client/lib/api-client.js';
 const FETCH_PROJECT = 'zooniverse-data/FETCH_PROJECT';
 const FETCH_PROJECT_SUCCESS = 'zooniverse-data/FETCH_PROJECT_SUCCESS';
 const FETCH_PROJECT_ERROR = 'zooniverse-data/FETCH_PROJECT_ERROR';
+const FETCH_WORKFLOW = 'zooniverse-data/FETCH_WORKFLOW';
+const FETCH_WORKFLOW_SUCCESS = 'zooniverse-data/FETCH_WORKFLOW_SUCCESS';
+const FETCH_WORKFLOW_ERROR = 'zooniverse-data/FETCH_WORKFLOW_ERROR';
 
 const ZOODATA_STATUS = {
   IDLE: 'zooniverse-data/IDLE',
   FETCHING: 'zooniverse-data/FETCHING',
   SENDING: 'zooniverse-data/SENDING',
   SUCCESS: 'zooniverse-data/SUCCESS',
-  ERROR: 'zooniverse-data/SUCCESS',
+  ERROR: 'zooniverse-data/ERROR',
 };
 
 /*
@@ -33,11 +36,28 @@ const ZOODATA_STATUS = {
 // Reducer and Initial State
 // -------------------------
 
-const ZOODATA_INITIAL_STATE = {
+const DEFAULT_PROJECT_VALUES = {
   zooProjectId: null,
   zooProjectData: null,
   zooProjectStatus: ZOODATA_STATUS.IDLE,
   zooProjectStatusMessage: null,
+};
+
+const DEFAULT_WORKFLOW_VALUES = {
+  zooWorkflowId: null,
+  zooWorkflowData: null,
+  zooWorkflowStatus: ZOODATA_STATUS.IDLE,
+  zooWorkflowStatusMessage: null,
+};
+
+const DEFAULT_SUBJECT_VALUES = {};
+const DEFAULT_CLASSIFICATION_VALUES = {};
+
+const ZOODATA_INITIAL_STATE = {
+  ...DEFAULT_PROJECT_VALUES,
+  ...DEFAULT_WORKFLOW_VALUES,
+  ...DEFAULT_SUBJECT_VALUES,
+  ...DEFAULT_CLASSIFICATION_VALUES
 };
 
 const zoodataReducer = (state = ZOODATA_INITIAL_STATE, action) => {
@@ -50,7 +70,9 @@ const zoodataReducer = (state = ZOODATA_INITIAL_STATE, action) => {
         zooProjectStatusMessage: null,
 
         //Reset all Project dependencies
-        //TODO        
+        ...DEFAULT_WORKFLOW_VALUES,
+        ...DEFAULT_SUBJECT_VALUES,
+        ...DEFAULT_CLASSIFICATION_VALUES
       });
     case FETCH_PROJECT_SUCCESS:
       return Object.assign({}, state, {
@@ -61,10 +83,33 @@ const zoodataReducer = (state = ZOODATA_INITIAL_STATE, action) => {
     case FETCH_PROJECT_ERROR:
       return Object.assign({}, state, {
         zooProjectData: action.projectData,
-        zooProjectStatus: ZOODATA_STATUS.SUCCESS,
+        zooProjectStatus: ZOODATA_STATUS.ERROR,
         zooProjectStatusMessage: action.statusMessage,
       });
-    
+    //----- Workflows
+    case FETCH_WORKFLOW:
+      return Object.assign({}, state, {
+        zooWorkflowId: action.workflowId,
+        zooWorkflowData: null,
+        zooWorkflowStatus: ZOODATA_STATUS.FETCHING,
+        zooWorkflowStatusMessage: null
+
+        //Reset all Project dependencies
+        //TODO
+      });
+    case FETCH_WORKFLOW_SUCCESS:
+      return Object.assign({}, state, {
+        zooWorkflowData: action.workflowData,
+        zooWorkflowStatus: ZOODATA_STATUS.SUCCESS,
+        zooWorkflowStatusMessage: null
+      });
+    case FETCH_WORKFLOW_ERROR:
+      return Object.assign({}, state, {
+        zooWorkflowData: action.workflowData,
+        zooWorkflowStatus: ZOODATA_STATUS.ERROR,
+        zooWorkflowStatusMessage: action.statusMessage
+      });
+
     default:
       return state;
   };
@@ -78,25 +123,51 @@ const zoodataReducer = (state = ZOODATA_INITIAL_STATE, action) => {
 // ---------------
 
 const ZooData =  {
-  fetchProject: (projectId) => {
+  fetchProject: (projectId, onSuccess = () => {}, onError = () => {}) => {
     return (dispatch) => {
-      
+
       //Store update: enter "fetching" state.
       dispatch({ type: FETCH_PROJECT, projectId });
-      
+
       //Asynchronous action
       apiClient.type('projects').get(projectId)
-        .then((project)=>{
+        .then((project) => {
           //Store update: enter "success" state and save fetched data.
           dispatch({ type: FETCH_PROJECT_SUCCESS, projectData: project });
+
+          onSuccess();
         })
-      
-        .catch((err)=>{
+
+        .catch((err) => {
           //Store update: enter "error" state.
           dispatch({ type: FETCH_PROJECT_ERROR, statusMessage: err });
+
+          onError();
         });
     }
   },
+  fetchWorkflow: (workflowId, onSuccess = () => {}, onError = () => {}) => {
+    return (dispatch) => {
+      //Store update: enter "fetching" state.
+      dispatch({ type: FETCH_WORKFLOW, workflowId });
+
+      //Asynchronous action
+      apiClient.type('workflows').get(workflowId)
+        .then((workflow)=>{
+          //Store update: enter "success" state and save fetched data.
+          dispatch({ type: FETCH_WORKFLOW_SUCCESS, workflowData: workflow });
+
+          onSuccess();
+        })
+
+        .catch((err)=>{
+          //Store update: enter "error" state.
+          dispatch({ type: FETCH_WORKFLOW_ERROR, statusMessage: err });
+
+          onError();
+        });
+      };
+  }
 };
 
 /*
